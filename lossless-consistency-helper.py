3d14a86bc6f27b2f4dafd43a5fd366cc53713d6b
Michael P. Geraci <me@mgeraci.com>
2018-01-16 22:11:37 -0500

    successfully return and print images that are not called "cover"

--- lossless-consistency-helper.py
+++ lossless-consistency-helper.py
@@ -5,16 +5,46 @@ import os
 import fnmatch
 
 
+# helpers
+# ------------------------------------------------------------------------------
+
+def add_error_to_res(path, error):
+    if not res.get(path):
+        res[path] = []
+
+    res[path].append(error)
+
+
+def check_filename(path):
+    is_good_png = 'cover.png' in path
+    is_good_jpg = 'cover.jpg' in path
+    is_good_jpeg = 'cover.jpeg' in path
+
+    if is_good_png or is_good_jpg or is_good_jpeg:
+        return {
+            'success': True,
+        }
+    else:
+        return {
+            'success': False,
+            'error': 'Bad filename'
+        }
+
+
+# script
+# ------------------------------------------------------------------------------
+
 root = '/Volumes/Lossless/Lossless'
 images = []
-
+res = {}
 
 print 'xxxxxxxxxxxxx'
 print 'x hey there x'
 print 'xxxxxxxxxxxxx'
 print ''
-print 'Finding images...'
 
+
+print 'Finding images...'
 
 os.chdir(root)
 
@@ -27,12 +57,16 @@ for root, dirnames, filenames in os.walk(root):
         if is_png or is_jpg or is_jpeg:
             images.append(os.path.join(root, filename))
 
-
 print '{} images found'.format(len(images))
 print ''
 
 
-def check_filename(path):
-    return {
-        'success': True,
-    }
+print 'Looking for images named something other than "cover"...'
+
+for image in images:
+    filename_check = check_filename(image)
+
+    if not filename_check['success']:
+        add_error_to_res(image, filename_check['error'])
+
+print res
