62d1e664e916429de27d79f953cbdcd29ea2ae07
Michael P. Geraci <me@mgeraci.com>
2018-01-17 20:33:14 -0500

    remove image status as you go; it is fast enough

--- lossless-consistency-helper.py
+++ lossless-consistency-helper.py
@@ -140,8 +140,6 @@ for image in images:
     if not image_check['success']:
         add_error_to_res('images', image, image_check['error'])
 
-    print("."),
-
 
 # write the output
 # ------------------------------------------------------------------------------
@@ -158,7 +156,6 @@ with open('{}/output.txt'.format(output_dir), 'w') as f:
 
         f.write('\n')
 
-print ''
 print ''
 print 'Done! Check your results in output.txt'
 print '<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3<3'
