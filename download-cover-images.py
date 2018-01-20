f31b2440b6cdf5e5aed24c88f2ce4cf5b9c1c723
Michael P. Geraci <me@mgeraci.com>
2018-01-20 17:56:41 -0500

    make the rest of the downloader

--- download-cover-images.py
+++ download-cover-images.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import json
+import requests
 from localsettings import MUSIC_LOCATION
 
 output_dir = os.path.dirname(os.path.realpath(__file__))
@@ -11,6 +12,41 @@ cover_dir = '{}/covers/'.format(output_dir)
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
+
+# helpers
+# ------------------------------------------------------------------------------
+
+def get_name_list_from_filename(path):
+    '''
+    Returns an list of [artist_name, album_name] from a file path.
+
+    @param {str} path - the path of the album
+    @return {list} - a list of [artist, album]
+    '''
+
+    res = path.replace(MUSIC_LOCATION, '')
+
+    if res[0] == '/':
+        res = res[1:]
+
+    res = res.split('/')
+
+    # discard any "cover.jpg", if present
+    res = res[:2]
+
+    return res
+
+def download_image(folder, url):
+    destination = '{}cover.png'.format(folder)
+    print 'downloading {}'.format(destination)
+
+    f = open(destination,'wb')
+    f.write(requests.get(url).content)
+    f.close()
+
+
+# the script
+# ------------------------------------------------------------------------------
 
 import_file = '{}/{}'.format(output_dir, 'get-new-images-output.txt')
 
@@ -43,4 +79,20 @@ for image in data:
     if not len(image_info['result']):
         continue
 
-    print image_info['result']
+    artist, album = get_name_list_from_filename(image)
+    url = image_info['result']
+
+    artist_dir = u'{}/covers/{}'.format(output_dir, artist)
+    album_dir = u'{}/{}'.format(artist_dir, album)
+
+    if not os.path.exists(artist_dir):
+        os.makedirs(artist_dir)
+
+    if not os.path.exists(album_dir):
+        os.makedirs(album_dir)
+
+    download_image(album_dir, url)
+
+print ''
+print 'Done! That wasn\'t too bad, right?'
+print ''
